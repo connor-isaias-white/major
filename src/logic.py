908a5bda7522e0f0a91bc6d8b2ac1f119cc97706
Connor <connor.isaiaswhite@gmail.com>
2020-01-23 10:20:05 +1100

    added some sight to the players

--- src/logic.py
+++ src/logic.py
@@ -1,26 +1,43 @@
 import pygame
 from src.player import player
 from config import config
+import time
 
 class logic:
     def __init__(self, display):
         self.display = display
         self.running = True
+        self.width = config['screen']['width']
+        self.height = config['screen']['height']
 
     def loop(self):
         self.setup()
         clock = pygame.time.Clock()
         while self.running:
             self.events()
+
+            self.seaker.decide(self.hider.pos)
+            self.hider.decide(self.seaker.pos)
+            self.checkwin()
+
             pygame.display.update()
             clock.tick(config['screen']['fps'])
 
     def setup(self):
-        seaker = player([50,50])
-        seaker.draw(self.display)
+        self.startTime = time.time()
+        self.seaker = player([100,100], (255, 0, 0), self.display, "seaker")
+        self.hider = player([400,400], (0, 255, 0), self.display, "hider")
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
                 exit()
+
+    def checkwin(self):
+        if self.seaker.pos == self.hider.pos:
+            frames = int((time.time() - self.startTime)*config["screen"]["fps"])
+            print(f"seaker won after {frames} frames")
+        for i in [self.seaker, self.hider]:
+            if i.pos[0] < 0 or i.pos[1] < 0 or i.pos[0] > self.width or i.pos[1] > self.height:
+                print(f"{i.name} hit wall")
--- src/player.py
+++ src/player.py
@@ -1,9 +1,45 @@
 import pygame
+from config import config
+import random
+
 class player:
     radius = 5
 
-    def __init__(self, pos):
+    def __init__(self, pos, colour, display, name):
         self.pos = pos
+        self.colour = colour
+        self.display = display
+        self.name = name
 
-    def draw(self, display):
-        pygame.draw.circle(display, [255,0,0], self.pos, self.radius)
+    def draw(self, colour):
+        pygame.draw.circle(self.display, colour, self.pos, self.radius)
+
+    def move(self, direction):
+        self.draw(0)
+
+        if direction == "up":
+            self.pos[1] -= 1
+        elif direction == "down":
+            self.pos[1] += 1
+        elif direction == "left":
+            self.pos[0] -= 1
+        elif direction == "right":
+            self.pos[0] += 1
+        else:
+            print("not a direction")
+
+        self.draw(self.colour)
+
+    def see(self, enemyPos):
+        upEye = self.pos[1]
+        downEye = config["screen"]["height"] -self.pos[1]
+        leftEye = self.pos[0]
+        rightEye = config["screen"]["width"] -self.pos[0]
+        enemyDist = ((self.pos[0] - enemyPos[0])**2 + (self.pos[1] - enemyPos[1])**2)**(1/2)
+        # print(f"{self.name}: up: {upEye}, down: {downEye}, left: {leftEye}, right: {rightEye}, enemyPos: {enemyPos}, ownPos: {self.pos}, enemyDist: {enemyDist}")
+
+    def decide(self, enemyPos):
+        self.see(enemyPos)
+        directions = ["up", "down", "left", "right"]
+        choice = random.randint(0, 3)
+        self.move(directions[choice])
