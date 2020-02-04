7080261a0f48e818d9c35dcb57082a2206ad3572
connor <connor.white.spam@gmail.com>
2020-02-04 19:35:22 +1100

    increased object orientence

--- testing/testing.py
+++ testing/testing.py
@@ -11,6 +11,26 @@ config = {
     'initinputLength': 5,
 }
 
+class Network:
+    def __init__(self):
+        self.numlayers = random.randint(2, config['initinputLength'])
+        self.neuroninlayer = [random.randint(2, config['initinputLength']+1) for i in range(self.numlayers - 1)]
+        self.neuroninlayer.append(1)
+        self.layers = []
+        self.createNetwork()
+
+    def createNetwork(self):
+        for hiddenlayer in range(self.numlayers):
+            layer = []
+            for neuro in range(self.neuroninlayer[hiddenlayer]):
+                if hiddenlayer != 0:
+                    inputLength = self.neuroninlayer[hiddenlayer-1]
+                else:
+                    inputLength = config['initinputLength']
+                layer.append(neuron(neuro,inputLength))
+            self.layers.append(layer)
+
+
 class neuron:
 
     def __init__(self, pos, inputLength):
@@ -37,26 +57,14 @@ def train():
     run = 0
     bestPer= [0,0]
     while perfectNotFount:
-        numlayers = random.randint(2, config['initinputLength'])
-        neuroninlayer = [random.randint(2, config['initinputLength']+1) for i in range(numlayers - 1)]
-        neuroninlayer.append(1)
-        # print(f'{numlayers-1} hidden layers and setup like {neuroninlayer}')
-        layers = []
-        for hiddenlayer in range(numlayers):
-            layer = []
-            for neuro in range(neuroninlayer[hiddenlayer]):
-                if hiddenlayer != 0:
-                    inputLength = neuroninlayer[hiddenlayer-1]
-                else:
-                    inputLength = config['initinputLength']
-                layer.append(neuron(neuro,inputLength))
-            layers.append(layer)
-
-        percent = network(trainData, layers)
-        if percent >= 95 or percent ==0:
-            print(f'correct: {percent}%, after {run} tries, {numlayers-1} hidden layers and setup like {neuroninlayer}')
-            perfectNotFount = False
-            return layers
+        newNetwork = Network()
+        percent = network(trainData, newNetwork.layers)
+        if percent == 100 or percent ==0:
+            print(f'correct: {percent}%, after {run} tries, {newNetwork.numlayers-1} hidden layers and setup like {newNetwork.neuroninlayer}')
+            testResults = test(newNetwork)
+            if testResults == 100 or testResults== 0:
+                perfectNotFount = False
+                return newNetwork
         elif percent > bestPer[0]:
             bestPer=[percent,1]
         elif percent == bestPer[0]:
@@ -84,7 +92,7 @@ def putInNetwork(input, layers):
 
 
 def test(best):
-    final = network(testData, best)
+    final = network(testData, best.layers)
     print(f"testing score: {final}%")
     return final
 
@@ -111,7 +119,7 @@ def createData():
 
 def plot(network):
     master = tk.Tk()
-
+    network = network.layers
     canvas_width = 720
     canvas_height = 720
     w = tk.Canvas(master,
@@ -123,7 +131,7 @@ def plot(network):
     for layer in range(len(network)):
         totalNerons = len(network[layer])
         for neron in range(len(network[layer])):
-            distance = canvas_height/(config['initinputLength']*2-totalNerons+1)
+            distance = canvas_height/(config['initinputLength']*3-totalNerons+1)
             colour = colour = "#"+''.join([hex(random.randint(15, 255))[2:] for i in range(3)])
             x = int(((layer+1)/(len(network)+1))*canvas_width)+10
             y = int( (canvas_height/2) +((-1)**(neron+1)) *distance*(int((neron+1+((totalNerons+1) % 2))/2) ))
@@ -132,7 +140,7 @@ def plot(network):
                 radius = network[layer][neron].sig(network[layer][neron].bias)*10+5
                 for input in range(len(network[layer-1])):
                     weight = network[layer][neron].sig(network[layer][neron].weights[input])
-                    distance2 = canvas_height/(config['initinputLength']*2-len(network[layer-1])+1)
+                    distance2 = canvas_height/(config['initinputLength']*3-len(network[layer-1])+1)
                     x2 = int(((layer)/(len(network)+1))*canvas_width)+10
                     y2 = int( (canvas_height/2) +((-1)**(input+1)) *distance2*(int((input+1+((len(network[layer-1])+1) % 2))/2) ))
                     w.create_line(x2,y2,x,y, width = weight*10, fill=colour)
@@ -147,21 +155,21 @@ def circle(canvas,x,y, r, colour):
 
 if __name__ == "__main__":
     trainData, testData = createData()
+    while len(testData)*len(trainData) == 0:
+        trainData, testData = createData()
+    print(f"train data length: {len(trainData)}, test data length: {len(testData)}")
 
     best = train()
-    if best != 0:
-        final = test(best)
-
-        if final >= 95 or final == 0:
-            plot(best)
-            '''print(f"input {config['initinputLength']} 1s or 0s seperated by commas and the network will tell if 2 1s are ajacent")
-            manData = input("> ")
-            while manData:
-                manData = manData.replace(" ", '').split(",")
-                data = [int(i) for i in manData]
-                output = round(putInNetwork(data, best)[0])
-                if output == final/100:
-                    print("at least 2 adjacent 1s")
-                else:
-                    print("No adjacent 1s")
-                manData = input("> ")'''
+    if best != []:
+        plot(best)
+        '''print(f"input {config['initinputLength']} 1s or 0s seperated by commas and the network will tell if 2 1s are ajacent")
+        manData = input("> ")
+        while manData:
+            manData = manData.replace(" ", '').split(",")
+            data = [int(i) for i in manData]
+            output = round(putInNetwork(data, best)[0])
+            if output == final/100:
+                print("at least 2 adjacent 1s")
+            else:
+                print("No adjacent 1s")
+            manData = input("> ")'''
