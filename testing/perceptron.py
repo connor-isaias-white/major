2f72af70934fca6ece8a4d6e20b073917265ca2d
connor <connor.white.spam@gmail.com>
2020-02-24 10:58:34 +1100

    finished basic linear regression

--- testing/perceptron.py
+++ testing/perceptron.py
@@ -1,103 +1,83 @@
 import random
 import math
-import tkinter as tk
 import time
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 config = {
-        "width": 255,
-        "height": 255,
+        "size":[5,5],
     }
 
 class perceptron:
-    def __init__(self, learningRate):
+    def __init__(self, learningRate, trainData):
         self.weights = [random.random(), random.random()]
-        self.bias = random.random()
+        self.bias = 0
         self.learningRate = learningRate
-
+        self.trainData = trainData
     def sig(self, x):
         y = 1/(1+math.exp(-x))
         return y
 
     def learn(self, guess, point):
+        #print(self.weights)
         ans = point[1]
+        #print(ans, guess)
         error = ans - guess
         for i in range(len(self.weights)):
             self.weights[i] += error*point[0][i] * self.learningRate
+        self.bias += error *self.learningRate
 
     def guess(self, point):
-        output = self.bias
+        output = 0
         for i in range(len(point[0])):
-            output += self.weights[i]*point[0][i]
-        output = round(self.sig(output))
-        if output != point[1]:
+            output += self.weights[i]*(point[0][i])
+        output = self.sig(output+self.bias)
+        if round(output) != point[1]:
             self.learn(output, point)
-        return (output == point[1])
-
-class screen:
-    def __init__(self, points):
-        self.width = config["width"]
-        self.height = config["height"]
-        master = tk.Tk()
-        self.w =tk.Canvas(master, width=config["width"], height=config["height"]) 
-        self.line= self.w.create_line(0,0,self.width,self.height)
-        self.w.pack()
-        self.points = points
-
-    def circle(self, x,y,r, colour):
-        self.w.create_oval(x-r,y-r,x+r,y+r, fill=colour)
-    
-    def update(self):
-        self.w.update_idletasks()
-        self.w.update()
-    
-    def drawPoints(self):
-        for point in self.points:
-            if point[1]==1:
-                colour = "#00ff00"
-            else:
-                colour="#ff0000"
-            self.circle(round(point[0][0]*config["width"]), round(point[0][1]*config["height"]), 3, colour)
-
-    def drawLine(self, rise, run):
-        #self.w.delete("all")
-        #self.drawPoints()
-        gradient = rise/run
-        self.w.create_line(0,self.lineEquation(gradient, 0), self.width, self.lineEquation(gradient, self.width))
-        #self.w.update()
-
-    def lineEquation(self, m,x):
-        y=m*x
-        return y
+        return (round(output) == point[1])
 
 def createData(num):
     data = []
+    grad = random.uniform(-10,10)
+    interspt = random.uniform(-1, 1)
     for point in range(num):
-        x = random.random()
-        y = random.random()
-        if y > x:
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)
+        #if y <= grad*x+interspt:
+        if y <= x**2:
             val = 1
         else:
             val = 0
         data.append([[x,y], val])
     return data
 
-def train(trainData, Screen):
-    percy = perceptron(0.1)
-    for i in range(10):
-        total = 0
-        for point in trainData:
-            correct = percy.guess(point)
-            if correct:
-                total += 1
-        Screen.update()
-        Screen.drawLine(percy.weights[0], percy.weights[1])
-        Screen.update()
-        #time.sleep(1)
-        #print(f"{total/len(trainData)}%")
+def train():
+    total = 0
+    for point in percy.trainData:
+        correct = percy.guess(point)
+        if correct:
+            total += 1
+    b = percy.bias#-(int(percy.bias>0)*2-1)
+    line = ax.plot([-config["size"][0],config["size"][0]],[-config["size"][0]*(-1*percy.weights[0]+b)/percy.weights[1],-config["size"][0]*(1*percy.weights[0]+b)/percy.weights[1]], c=[0,0,1])
+    return line, total/len(percy.trainData)
+
+def draw(data):
+    line = ax.plot([-config["size"][0],config["size"][0]], [0.75, -0.25])
+    ax.scatter([i[0][0]*config["size"][0] for i in data], [i[0][1]*config["size"][1] for i in data], c=[[0,i[1],0]for i in data])
+    ax.set_xlabel("")
+    ani = animation.FuncAnimation(fig, changeLine,)# blit=True)
+    plt.show()
+
+def changeLine(frame):
+    line, percent = train()
+    title = ax.set_xlabel(f'correct: {percent},  bias: {percy.bias},\n weights: {percy.weights}, intersept: {-percy.bias/percy.weights[1]}')
+    ax.lines = ax.lines[1:]
+    return line, title
 
 if __name__ == "__main__":
     trainData = createData(255)
-    Screen = screen(trainData)
-    Screen.drawPoints()
-    Screen.drawLine(1,2) 
-    train(trainData, Screen)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    percy = perceptron(0.1, trainData)
+    draw(trainData)
+
