ef8e35ef8f93b91a778a38ba589c45c5fc854ac7
connor <connor.white.spam@gmail.com>
2020-08-12 12:07:35 +1000

    deleted more unused files

--- handwritten/verifyData.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from pickle import load:
-
-def makeImg(dataz):
-    img = Image.new("L", (28, 28), color="black")
-    formed = np.resize(datz, (28,28)).tolist()
-    for x in range(len(dataz)):
-        for y in range(x):
-            img.putpixel((x,y), formed[x],[y])
